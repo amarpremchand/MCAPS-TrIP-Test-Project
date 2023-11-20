1e235cceb9275852b7358c4c1caef325a2439a5e
lologrignola <grignolabusiness@gmail.com>
2023-11-20 13:23:34 -0300

    Added MSAL auth

--- mcaps-trip/src/App.tsx
+++ mcaps-trip/src/App.tsx
@@ -1,16 +1,48 @@
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import React from "react";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import "./App.css";
 import Home from "./pages/Home/Home";
 import Movie from "./pages/Movie/Movie";
+import Login from "./pages/Login/Login";
+import { PublicClientApplication } from "@azure/msal-browser";
+import {
+  AuthenticatedTemplate,
+  MsalProvider,
+  UnauthenticatedTemplate,
+} from "@azure/msal-react";
+import { msalConfig } from "./authConfig";
+
+const msalInstance = new PublicClientApplication(msalConfig);
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route exact path="/" component={Home} />
-        <Route path="/movie/:id" component={Movie} />
-      </Switch>
-    </BrowserRouter>
+    <MsalProvider instance={msalInstance}>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            <UnauthenticatedTemplate>
+              <Redirect to="/login" />
+            </UnauthenticatedTemplate>
+            <AuthenticatedTemplate>
+              <Home />
+            </AuthenticatedTemplate>
+          </Route>
+          <Route path="/movie/:id">
+            <UnauthenticatedTemplate>
+              <Redirect to="/login" />
+            </UnauthenticatedTemplate>
+            <AuthenticatedTemplate>
+              <Movie />
+            </AuthenticatedTemplate>
+          </Route>
+          <Route path="/login" component={Login} />
+          {/* Redirect to /login if no other route matches */}
+          <Route path="*">
+            <Redirect to="/login" />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    </MsalProvider>
   );
 }
 
